c7f0e96e77996c99761d42aabd059d2b3d9769c9
seoyeonii <chaesy02@gmail.com>
2024-12-28 00:16:15 +0900

    [feat] 페이지 버튼 위치 수정

--- midnear/src/components/Shop/ProdList.jsx
+++ midnear/src/components/Shop/ProdList.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import Pagination from './Pagination';
 
 const ProdList = ({productList}) => {
-  const [limit, setLimit] = useState(4); // 한 페이지 당 보여줄 게시물 개수, 추후에 수정
+  const [limit, setLimit] = useState(11); // 한 페이지 당 보여줄 게시물 개수, 추후에 수정
   const [page, setPage] = useState(1); // 현재 페이지 위치
   const offset = (page -1) * limit; // 현재 페이지의 첫 번째 게시물 오프셋
 
@@ -15,7 +15,8 @@ const ProdList = ({productList}) => {
                 <img src={it.frontImg} className='frontImg'></img>
                 <img src={it.backImg} className='backImg'></img>
                 <p className='name'>{it.name}</p>
-                <p>{it.price}</p>
+                <p className='origin-price'>{it.price}</p>
+                <p className='dc-price'>{it.price}</p>
             </div>
         ))}
         
