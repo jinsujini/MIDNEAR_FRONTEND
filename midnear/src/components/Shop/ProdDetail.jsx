d281fe178cb10287bf765962a8ee786a7e7ac9ab
seoyeonii <chaesy02@gmail.com>
2025-01-01 19:47:25 +0900

    [feat] 디테일 정보 추가

--- midnear/src/components/Shop/ProdDetail.jsx
+++ midnear/src/components/Shop/ProdDetail.jsx
@@ -15,21 +15,24 @@ const ProdDetail = () => {
           backImg: backImg,
           name: "CUTE SWEATER",
           price: "\u20A9 39,900",
+          dcPrice: "\u20A9 35,100",
+          coupon: "10%할인쿠폰적용가",
+          soldout: "SOLD OUT"
         },
      ]
      const size = ['S', 'M', 'L', 'XL'];
      const information = [
       {
         name: 'DETAILS',
-        content: 'NONE'
+        content: 'Classic fit shirt in brushed cotton flannel.\n\n\nFeatures a patch pocket,\nfront button closure and topstitching along the seams.\nMidnear stock flag label stitched at the side.\n\n- Longsleeve\n- Collared neckline\n- Buttoned front closure and cuffs\n- Single patch pocket\n- Back yoke\n- Curved hem\n- Classic fit\n- Unisex\n- Material: 100% cotton\n- Imported'
       },
       {
         name: 'SIZE GUIDE',
-        content: 'NONE'
+        content: 'S- 어깨 48 기장 70 소매 64 가슴55\nM- 어깨 48 기장 70 소매 64 가슴55\nL- 어깨 48 기장 70 소매 64 가슴55\nXL- 어깨 48 기장 70 소매 64 가슴55'
       },
       {
         name: 'SHIPPING & RETURNS',
-        content: 'NONE'
+        content: '00택배 (1588-3223)\n\n구매하신 제품은 수령하신 날로부터 7일 이내에 접수해 주셔야 합니다.\n자세히 보기.'
       },
       {
         name: 'STYLED WITH',
